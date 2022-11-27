407a7942b9bc068e2ab383f3920f2ffeea1b3644
Niklas Furberg <niklas.furberg@dynabyte.se>
2022-11-27 21:11:54 +0100

    prints success/fail criteria

--- main.py
+++ main.py
@@ -19,21 +19,17 @@ def click(x, y):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
 
+def text(image_path):
+    path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    pytesseract.tesseract_cmd = path_to_tesseract
+    img = Image.open(image_path)
+    return pytesseract.image_to_string(img)
+
 screenshot('input', 1980, 1080)
 x = 100
 y = 100
 click(x, y)
 time.sleep(0.1)
 screenshot('output', 200, 200)
-
-#Define path to tessaract.exe
-path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-#Define path to image
-path_to_image = 'screenshots\\output.png'
-#Point tessaract_cmd to tessaract.exe
-pytesseract.tesseract_cmd = path_to_tesseract
-#Open image with PIL
-img = Image.open(path_to_image)
-#Extract text from image
-text = pytesseract.image_to_string(img)
-print(text)
+text = text('screenshots\\output.png')
+print("success" if 'output' in text else "fail")
