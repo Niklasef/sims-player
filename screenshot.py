fafdb3c5f4330b177ca5b0c142f29b3a2f658da2
Niklas Furberg <niklas.furberg@dynabyte.se>
2022-11-27 20:30:13 +0100

    Take HD screenshot and save to png

---
+++ screenshot.py
@@ -0,0 +1,8 @@
+import pygetwindow
+import time
+import os
+import pyautogui
+import PIL
+
+p = pyautogui.screenshot(region=(0,0, 1920, 1080))
+p.save(r'C:\\Users\\Niklas\\Projects\\sims-player\\pictures\\\\p.png')
