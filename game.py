1eadb84291f9a12ec7afbed6f53d93b2ea7787d6
Niklas Furberg <niklas.furberg@dynabyte.se>
2022-12-11 20:42:13 +0100

    renamed game file, refactored render logic, move click count into game state

--- game.py
+++ game.py
@@ -20,46 +20,51 @@ import random
 def inside(ox, oy, currentStateX):
     return ox > currentStateX and ox < (currentStateX+8) and oy > 320 and oy < 330
 
-def main():
-    click_count = 0
+# def step(state):
+
+def render(state):
+    os.system('cls')
+    print(str(state[1]))
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('________________(' + state[0][0] + ')____________(' + state[0][1] + ')_____________(' + state[0][2] + ')_______(' + state[0][3] + ')__')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('')
+    print('_______________________________________________________________')
+
+def run():
     state_left = win32api.GetKeyState(0x01)  # Left button down = 0 or 1. Button up = -127 or -128
     state_right = win32api.GetKeyState(0x02)  # Right button down = 0 or 1. Button up = -127 or -128
-    state = ['1', '2', '3', '4']
     positions = [162, 297, 442, 531]
-    random.shuffle(state)
+    
+    state = (['1', '2', '3', '4'], 0)
+    random.shuffle(state[0])
     current = 1
-    currentI = [i for i, x in enumerate(state) if int(x) == current][0]
+    currentI = [i for i, x in enumerate(state[0]) if int(x) == current][0]
     while True:
-        os.system('cls')
-        print(str(click_count))
-        print('')
-        print('')
-        print('')
-        print('')
-        print('')
-        print('')
-        print('')
-        print('')
-        print('')
-        print('')
-        print('')
-        print('')
-        print('')
-        print('________________(' + state[0] + ')____________(' + state[1] + ')_____________(' + state[2] + ')_______(' + state[3] + ')__')
-        print('')
-        print('')
-        print('')
-        print('')
-        print('')
-        print('')
-        print('')
-        print('')
-        print('')
-        print('')
-        print('')
-        print('')
-        print('')
-        print('_______________________________________________________________')
+        render(state)
         clicked = False
         if current > 4:
             return
@@ -68,16 +73,16 @@ def main():
             if a != state_left:  # Button state changed
                 state_left = a
                 if a >= 0:
-                    click_count += 1
+                    state[1] += 1
                     ox, oy = win32api.GetCursorPos()
                     isInside = inside(ox, oy, positions[currentI])
                     clicked = True
                     if isInside:
                         current += 1
-                        state[currentI] = 'X'
+                        state[0][currentI] = 'X'
                         if current <= 4:
-                            currentI = [i for i, x in enumerate(state) if x != 'X' and int(x) == current][0]
+                            currentI = [i for i, x in enumerate(state[0]) if x != 'X' and int(x) == current][0]
             time.sleep(0.001)
 
-main()
+run()
 print("Won")
